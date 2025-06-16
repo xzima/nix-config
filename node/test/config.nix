bd9e44ca442908a71c8eb596dad47a9a67624c73
Alex Zima <xzima@ro.ru>
2025-06-17 01:16:55 +0300

    feat(nixos): enable flakes

--- node/test/config.nix
+++ node/test/config.nix
@@ -3,6 +3,7 @@
   # Proxmox specific https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/virtualisation/proxmox-lxc.nix
   imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   #security.pam.services.sshd.allowNullPassword = true;
 
   system.stateVersion = "24.11";

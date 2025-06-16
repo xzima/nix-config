f415f60847567a5b5c51497e3f10c139131842a6
Alex Zima <xzima@ro.ru>
2025-06-17 02:34:31 +0300

    feat(nixos): configure global zsh config

--- node/test/config.nix
+++ node/test/config.nix
@@ -10,10 +10,6 @@
     pkgs.home-manager
   ];
 
-  users.users.root = {
-    #shell = pkgs.zsh;
-  };
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.

a422bf7cfa85b25c4fdb12ff1f6f6a29affb2ca0
Alex Zima <xzima@ro.ru>
2025-06-17 02:32:31 +0300

    feat(nixos): configure global zsh config

--- node/test/config.nix
+++ node/test/config.nix
@@ -4,15 +4,15 @@
   imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  programs.zsh.enable = true;
 
   environment.systemPackages = [
     pkgs.home-manager
   ];
 
   users.users.root = {
-    shell = pkgs.zsh;
+    #shell = pkgs.zsh;
   };
-  programs.zsh.enable = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release

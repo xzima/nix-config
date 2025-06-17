4eb30e00606064e072b6a7578b3ad06b8eb8b582
Alex Zima <xzima@ro.ru>
2025-06-17 22:03:41 +0300

    refactor: flake

--- node/test/home.nix
+++ node/test/home.nix
@@ -24,10 +24,11 @@
     initContent =
       let
         beforeCfg = lib.mkOrder 500 ''
+          # double single quotes (''$) to escape the dollar char
+          export HOST=''${HOST:-$(hostname)}
           # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
           # Initialization code that may require console input (password prompts, [y/n]
           # confirmations, etc.) must go above this block; everything else may go below.
-          # double single quotes (''$) to escape the dollar char
           if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
             source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
           fi

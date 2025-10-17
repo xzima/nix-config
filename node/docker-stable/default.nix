018d154c6efb7f2e65e7e5b5831e811eb87924aa
Alex Zima <xzima@ro.ru>
2025-10-17 23:36:03 +0300

    feat(docker-stable): enable ssh and configure gitea docker ssh forwarding
    - declare gitea-shell
    - set gitea-shell as git user shell
    - enable ssh daemon
    - configure docker ssh forwarding for gitea

--- node/docker-stable/default.nix
+++ node/docker-stable/default.nix
@@ -1,10 +1,4 @@
 { config, modulesPath, pkgs, lib, ... }:
-let
-  gitea-shell = pkgs.writeShellScriptBin "gitea-shell" ''
-    #!/bin/sh
-    ${pkgs.docker}/bin/docker exec -i --env SSH_ORIGINAL_COMMAND="''$SSH_ORIGINAL_COMMAND" gitea-container-name sh "''$@"
-  '';
-in
 {
   system.stateVersion = "25.05";
   # Flake specific
@@ -25,12 +19,26 @@ in
   # executed on configuration
   system.activationScripts = {
     # Gitea Docker SSH Forwarding
+    # ''$ - escape $ in nix file
+    # \''$ - escape $ in bash script in nix file
     gitea-docker-ssh-forwarding.text = ''
-      usermod -s ${gitea-shell}/bin/gitea-shell git
+      GITEA_SHELL_PATH="/usr/bin/gitea-shell"
+
+      ${pkgs.coreutils-full}/bin/rm -f ''$GITEA_SHELL_PATH
+
+      ${pkgs.coreutils-full}/bin/cat << EOF > "''$GITEA_SHELL_PATH"
+      #!/bin/sh
+      ${pkgs.docker}/bin/docker exec -i --env SSH_ORIGINAL_COMMAND="\''$SSH_ORIGINAL_COMMAND" gitea-container-name /bin/sh "\''$@"
+      EOF
+
+      ${pkgs.coreutils-full}/bin/chmod 755 ''$GITEA_SHELL_PATH
+      ${pkgs.shadow}/bin/usermod -s ''$GITEA_SHELL_PATH git
     '';
   };
   # protect ssh
   services.openssh = {
+    enable = true;
+    ports = [ 22 ];
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
@@ -40,10 +48,12 @@ in
       Macs = [ "hmac-sha2-256-etm@openssh.com" "hmac-sha2-256" ];
     };
     # Gitea Docker SSH Forwarding
+    # authorizedKeysCommand cannot reside in the /nix/store/...
+    # see https://github.com/NixOS/nixpkgs/issues/94653#issuecomment-673295418
     extraConfig = ''
       Match User git
         AuthorizedKeysCommandUser git
-        AuthorizedKeysCommand ${pkgs.docker}/bin/docker exec -i gitea-container-name /usr/local/bin/gitea keys -e git -u %u -t %t -k %k
+        AuthorizedKeysCommand /usr/bin/gitea-shell -c "/usr/local/bin/gitea keys -e git -u %u -t %t -k %k"
     '';
   };
   # packages https://search.nixos.org/packages
@@ -52,7 +62,6 @@ in
     pkgs.dig
     pkgs.unzip
     pkgs.ffmpeg
-    gitea-shell # Gitea Docker SSH Forwarding
   ];
   # Docker specific
   virtualisation.docker = {

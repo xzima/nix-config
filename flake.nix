887d6b3c50c34aaffe9d9167bb82e55037b2a6f2
Alex Zima <xzima@ro.ru>
2025-11-22 01:16:15 +0300

    TODO

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";

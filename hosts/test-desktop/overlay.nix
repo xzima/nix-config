7948ff9576cd20d2a42660983dacfa42b7527025
Alex Zima <xzima@ro.ru>
2026-01-30 06:13:03 +0300

    fix(test-desktop): freeze 'what's new..' and markdown preview in jetbrains idea

--- hosts/test-desktop/overlay.nix
+++ hosts/test-desktop/overlay.nix
@@ -1,15 +1,31 @@
-{ config, pkgs, lib, ... }:
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   nixpkgs.overlays = [
     (final: prev: {
-      jetbrains = prev.jetbrains // {
-        idea = prev.jetbrains.idea.overrideAttrs (old: rec {
-          src = prev.fetchurl {
-            url = builtins.replaceStrings [ "download.jetbrains.com" ] [ "download-cdn.jetbrains.com" ] (builtins.head old.src.urls);
-            sha256 = old.src.outputHash;
-          };
-        });
-      };
+      /*
+      fix freeze 'what's new..' and markdown preview
+      https://www.reddit.com/r/IntelliJIDEA/comments/1bvm4y0/intellij_crashing_after_last_update_when_trying/
+      https://github.com/NixOS/nixpkgs/issues/143122
+      https://youtrack.jetbrains.com/articles/JBR-A-11
+      Edit Custom Properties...
+      #ide.browser.jcef.gpu.disable=true
+      #ide.browser.jcef.sandbox.enable=false
+      ide.browser.jcef.enabled=false
+      */
+      jetbrains =
+        prev.jetbrains
+        // {
+          idea = prev.jetbrains.idea.overrideAttrs (old: rec {
+            src = prev.fetchurl {
+              url = builtins.replaceStrings ["download.jetbrains.com"] ["download-cdn.jetbrains.com"] (builtins.head old.src.urls);
+              sha256 = old.src.outputHash;
+            };
+          });
+        };
     })
   ];
 }

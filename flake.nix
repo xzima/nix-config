049489dbd09bc7f05d8ec923ababea04d5c1b5e0
Alex Zima <xzima@ro.ru>
2026-02-06 16:11:53 +0300

    feat(test-desktop): update flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.7.0";
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/?ref=v4.2.5";
+      url = "github:noctalia-dev/noctalia-shell/?ref=v4.3.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

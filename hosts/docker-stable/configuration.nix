d07abaf096ddbe8f2c93a5c709a279a506641cf5
Alex Zima <xzima@ro.ru>
2026-06-29 00:51:54 +0300

    fix(docker-stable): update nvidia driver

--- hosts/docker-stable/configuration.nix
+++ hosts/docker-stable/configuration.nix
@@ -31,12 +31,7 @@
       open = false; # Because video card is old
       gsp.enable = false;
       nvidiaPersistenced = true;
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "580.126.09"; # Should be equal with proxmox host version
-        sha256_64bit = "sha256-TKxT5I+K3/Zh1HyHiO0kBZokjJ/YCYzq/QiKSYmG7CY=";
-        settingsSha256 = "sha256-4SfCWp3swUp+x+4cuIZ7SA5H7/NoizqgPJ6S9fm90fA=";
-        persistencedSha256 = "sha256-J1UwS0o/fxz45gIbH9uaKxARW+x4uOU1scvAO4rHU5Y=";
-      };
+      package = config.boot.kernelPackages.nvidiaPackages.production; # https://github.com/NixOS/nixpkgs/blob/nixos-25.11/pkgs/os-specific/linux/nvidia-x11/default.nix#L74
     };
   };
 

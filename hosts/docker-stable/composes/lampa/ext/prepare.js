3d23e6991b4f487d857a7b12d419211b27732970
Alex Zima <xzima@ro.ru>
2026-07-06 03:49:31 +0300

    fix(docker-stable): lampa project
    - change schema to https in config
    - add accessControlAllowHeaders configuration for qbittorrent
    - add prepare.js plugin for configure client
    - update import plugin list in config

---
+++ hosts/docker-stable/composes/lampa/ext/prepare.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    Lampa.Storage.set('proxy_tmdb', true);
+    Lampa.Storage.set('source', 'tmdb');
+    Lampa.Storage.set("lmetorrentqBittorentUrl", "https://qbittorrent.az-101.myaddr.io");
+    Lampa.Storage.set("lmetorrentqBittorentTweak", true);
+    Lampa.Storage.set("lmetorrentqBittorentTV", "tv-sonarr");
+    Lampa.Storage.set("lmetorrentqBittorentMovies", "radarr");
+})();
